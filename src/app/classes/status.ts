80c5df5c2abd14037a8105aad90440557e0607db
yohanes <yohanesabing@gmail.com>
2022-10-20 22:31:25 +0700

    Adding remaining atribut in status

--- src/app/classes/status.ts
+++ src/app/classes/status.ts
@@ -1,9 +1,10 @@
-import { User } from "./user";
+import { User } from './user';
 
 export class Status {
   id!: number;
   status!: string;
   owe_amount!: number;
+  remaining_amount!: number;
   owe!: User;
   user!: User;
 }
