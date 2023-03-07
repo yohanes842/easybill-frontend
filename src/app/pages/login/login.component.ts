3de416cffb78b1ed0806b9e95a337f490c309b7f
yohanes <yohanesabing@gmail.com>
2023-03-07 18:22:47 +0700

    Refactor Login Component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -14,47 +14,50 @@ import { CustomMessageService } from 'src/app/services/message/custom-message.se
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  user: LoginForm = new LoginForm();
+  user: LoginForm;
 
   constructor(
     private authService: AuthService,
     private messageService: CustomMessageService,
     private router: Router
-  ) {}
+  ) {
+    this.user = new LoginForm();
+  }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
-  submitLogin(): void {
+  submitLogin() {
     if (!this.user.username || !this.user.password) {
       this.messageService.showMessage(
         Severity.ERROR,
         'INPUT ERROR',
         'Username/password can not be empty'
       );
-    } else
-      this.authService.login(this.user).subscribe(
-        () => {
-          this.router.navigateByUrl(Route.HOME_PATH);
-          this.messageService.showMessage(Severity.SUCCESS, 'LOGIN SUCCESS');
-        },
-        (err: ErrorResponse) => {
-          let { code, message } = err;
-          let errMsg;
-          switch (code) {
-            case ResponseStatus.USER_NOT_FOUND:
-            case ResponseStatus.INVALID_CREDENTIALS:
-              errMsg = 'Username or password invalid!';
-              break;
-            default:
-              errMsg = message;
-              break;
-          }
-          this.messageService.showMessage(
-            Severity.ERROR,
-            'LOGIN ERROR',
-            errMsg
-          );
+      return;
+    }
+    this.authService.login(this.user).subscribe({
+      next: () => {
+        this.router.navigateByUrl(Route.HOME_PATH);
+        this.messageService.showMessage(Severity.SUCCESS, 'LOGIN SUCCESS');
+      },
+      error: (error: ErrorResponse) => {
+        let { code, message } = error;
+        let errorMessage;
+        switch (code) {
+          case ResponseStatus.USER_NOT_FOUND:
+          case ResponseStatus.INVALID_CREDENTIALS:
+            errorMessage = 'Username or password invalid!';
+            break;
+          default:
+            errorMessage = message;
+            break;
         }
-      );
+        this.messageService.showMessage(
+          Severity.ERROR,
+          'LOGIN ERROR',
+          errorMessage
+        );
+      },
+    });
   }
 }
